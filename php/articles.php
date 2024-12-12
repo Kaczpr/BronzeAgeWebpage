12ecc65e03bcf71f1f27fd28705cd36220946cd5
Kaczpr <bojarski.kacper02@gmail.com>
2024-12-12 19:09:32 +0100

    CivSelection
    changed

--- php/articles.php
+++ php/articles.php
@@ -11,6 +11,8 @@
     href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
     rel="stylesheet" />
   <link rel="stylesheet" href="../css/articlesDisplayStyle.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 
 <body>
@@ -69,13 +71,11 @@
 
             }
             ?>
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+
           </ul>
         </div>
         <div class="readMore">
-          Znajdź więcej artykułów na ten temat
+        Znajdź więcej artykułów na ten temat &rarr;  
         </div>
       </div>
     </div>
@@ -86,13 +86,10 @@
       <div class="articlesList">
         <div class="oneArticle">
           <ul class="articlesList">
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
           </ul>
         </div>
         <div class="readMore">
-          Znajdź więcej artykułów na ten temat
+          Znajdź więcej artykułów na ten temat &rarr;
         </div>
       </div>
     </div>
@@ -103,13 +100,10 @@
       <div class="articlesList">
         <div class="oneArticle">
           <ul class="articlesList">
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
           </ul>
         </div>
         <div class="readMore">
-          Znajdź więcej artykułów na ten temat
+          Znajdź więcej artykułów na ten temat &rarr;
         </div>
       </div>
     </div>
@@ -120,13 +114,10 @@
       <div class="articlesList">
         <div class="oneArticle">
           <ul class="articlesList">
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
           </ul>
         </div>
         <div class="readMore">
-          Znajdź więcej artykułów na ten temat
+          Znajdź więcej artykułów na ten temat &rarr;
         </div>
       </div>
     </div>
@@ -137,13 +128,10 @@
       <div class="articlesList">
         <div class="oneArticle">
           <ul class="articlesList">
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-            <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
           </ul>
         </div>
         <div class="readMore">
-          Znajdź więcej artykułów na ten temat
+          Znajdź więcej artykułów na ten temat &rarr;
         </div>
       </div>
     </div>
